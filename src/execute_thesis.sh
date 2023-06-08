8bd9d9db73e1f54e8260f4bfd1fae602ea28c90a
Christian Bromley <christian.bromley@benevolent.ai>
2023-06-08 18:11:17 +0100

    feat: adding tokenizer code

--- src/execute_thesis.sh
+++ src/execute_thesis.sh
@@ -3,7 +3,8 @@
 export PDF_PATH=/Users/christianbromley/Documents/Personal/PhD/Christian_Bromley_Final_Thesis_20211221_cover.pdf
 export OUTPUT_PATH=/Users/christianbromley/Documents/Personal/PhD/Christian_Bromley_Final_Thesis_20211221.csv
 export TEXT_PARSE_DIR=/Users/christianbromley/Documents/Personal/PhD/thesis_parsed
+export MODELS_DIR=/Users/christianbromley/Documents/Projects/machine-learning-course/bert-transformer/models
 
 python3 /Users/christianbromley/Documents/Projects/machine-learning-course/bert-transformer/src/pdf_to_txt.py ${PDF_PATH} ${OUTPUT_PATH} --start_page 13 --end_page 139
 python3 /Users/christianbromley/Documents/Projects/machine-learning-course/bert-transformer/src/prepare_text_samples.py ${OUTPUT_PATH} ${TEXT_PARSE_DIR}
-
+python3 /Users/christianbromley/Documents/Projects/machine-learning-course/bert-transformer/src/tokenize.py ${TEXT_PARSE_DIR} ${MODELS_DIR}
---
+++ src/tokenize.py
@@ -0,0 +1,54 @@
+import argparse
+from pathlib import Path
+#import sys
+#sys.path.insert(0,'./.env/lib/python3.8/site-packages')
+from tokenizers import ByteLevelBPETokenizer
+import os
+
+
+# Create the parser
+parser = argparse.ArgumentParser(description='Tokenize text')
+
+# Add arguments
+parser.add_argument('input_dir', type=str, help='Directory with your input text files')
+parser.add_argument('output_dir', type=str, help='Directory to save model')
+
+
+def parse_arguments():
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Access the arguments
+    input_dir = args.input_dir
+    output_dir = args.output_dir
+
+    return input_dir, output_dir
+
+def extract_file_paths(input_dir):
+    paths = [str(x) for x in Path(input_dir).glob('**/*.txt')]
+    return paths
+
+def run(paths, output_dir):
+    # intialise tokeniser
+    tokenizer = ByteLevelBPETokenizer()
+    # train
+    tokenizer.train(files=paths,
+                    vocab_size=30_522,
+                    min_frequency=2,
+                    special_tokens=['<s>', '<pad>', '</s>', '<unk>', '<mask>'])
+
+    tokenizer.save_model(output_dir)
+
+def main():
+    # create directory for the text files
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    # get file paths
+    paths = extract_file_paths(input_dir)
+
+    # now execute
+    run(paths, output_dir)
+
+if __name__ == "__main__":
+     main()
