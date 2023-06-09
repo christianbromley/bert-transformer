f507d6dd5f3ad749069241dc07198288f20fe037
Christian Bromley <christian.bromley@benevolent.ai>
2023-06-09 15:10:06 +0100

    fix: minor fixes to the bash script

--- src/execute_thesis.sh
+++ src/execute_thesis.sh
@@ -5,6 +5,18 @@ export OUTPUT_PATH=/Users/christianbromley/Documents/Personal/PhD/Christian_Brom
 export TEXT_PARSE_DIR=/Users/christianbromley/Documents/Personal/PhD/thesis_parsed
 export MODELS_DIR=/Users/christianbromley/Documents/Projects/machine-learning-course/bert-transformer/models
 
+# first convert the PDF to text
 python3 /Users/christianbromley/Documents/Projects/machine-learning-course/bert-transformer/src/pdf_to_txt.py ${PDF_PATH} ${OUTPUT_PATH} --start_page 13 --end_page 139
+echo "PDF to text completed"
+
+# next prepare the text samples
 python3 /Users/christianbromley/Documents/Projects/machine-learning-course/bert-transformer/src/prepare_text_samples.py ${OUTPUT_PATH} ${TEXT_PARSE_DIR}
+echo "Text prepared"
+
+# create the tokenizer
 python3 /Users/christianbromley/Documents/Projects/machine-learning-course/bert-transformer/src/tokenize.py ${TEXT_PARSE_DIR} ${MODELS_DIR}
+echo "Tokenizer created"
+
+# now train
+python3 /Users/christianbromley/Documents/Projects/machine-learning-course/bert-transformer/src/input_pipeline.py ${MODELS_DIR} ${TEXT_PARSE_DIR} ${MODELS_DIR}
+echo "Model trained and saved"
